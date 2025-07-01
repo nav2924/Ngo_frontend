fed81a198538b0742e79e235cb6da21993f85b38
nav2924 <naveen29012004@gmail.com>
2025-07-01 20:05:38 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="text-3xl font-bold text-blue-500 underline">
+      Tailwind is working!
+    </div>
+
+  )
+}
+
+export default App
